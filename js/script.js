3b51e0b82d185c9a70aa8552bd99709d1525dfc2
Maxim Dmitruk <39312751+SimaDmtr@users.noreply.github.com>
2018-07-20 03:48:38 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,71 @@
 $(document).ready(function () {
-   $('.newsHeader').on('click', function () {
-       var $this = $(this),
-           item = $this.closest('.item_news'),
-           list = $this.closest('.newsList'),
-           items = list.find('item_news'),
-           content = item.find('.newsContent'),
-           otherContent = list.find('.newsContent'),
-           duration = 300;
-       otherContent.slideUp(duration);
-       content.slideDown(duration);
-   });
+
+// аккордион новостей
+    var mainAccord = $('.item_news');
+    mainAccord.find('.newsHeader:not(.active)').siblings('div').stop().slideUp();
+    mainAccord.find('.newsHeader').on('click', function () {
+        $('.newsHeader').removeClass('active');
+        $(this).addClass('active');
+        mainAccord.find('.newsHeader:not(.active)').siblings('.newsContent').stop().slideUp();
+        mainAccord.find('.newsHeader.active').siblings('.newsContent').stop().slideDown();
+    });
+//аккордион FAQ
+    var accordFaq = $('.questLeft > div, .questRight > div');
+    accordFaq.find('.questionItem:not(.active)').siblings('div').stop().slideUp();
+    accordFaq.find('.questionItem').on('click', function () {
+        $(this).find('.questionMark').css({background: 'rgba(1,1,1,0'});
+        $('.questionItem').removeClass('active').css({background: 'linear-gradient(to right, rgba(80,189,239,1) 0%,rgba(128,97,220,1) 100%)'});
+        $(this).addClass('active');
+        accordFaq.find('span').css({color: '#ffffff'})
+        accordFaq.find('.questionItem:not(.active)').css({background: 'transparent'});
+        accordFaq.find('.questionItem:not(.active)').find('span').css({color: 'inherit'})
+        accordFaq.find('.questionItem:not(.active)').siblings('.answer').stop().slideUp();
+        accordFaq.find('.questionItem:not(.active)').find('.questionMark').css({background: 'linear-gradient(122deg, rgba(80,189,239,1) 0%,rgba(128,97,220,1) 100%)'});
+        accordFaq.find('.questionItem.active').siblings('.answer').stop().slideDown();
+    });
+
+
+// слайдер
+    var $range = $("#depCalc"),
+        $input = $("#sum"),
+        instance,
+        min = 0,
+        max = 1000;
+
+    $range.ionRangeSlider({
+        grid_num: 51,
+        min: 51,
+        max: 100,
+        from: 83,
+        grid: true,
+        postfix: "$",
+        onStart: function (data) {
+            $input.prop("value", data.from);
+        },
+        onChange: function (data) {
+            $input.prop("value", data.from);
+        }
+    });
+
+    instance = $range.data("ionRangeSlider");
+
+    $input.on("change keyup", function () {
+        var val = $(this).prop("value");
+        if (val < min) {
+            val = min;
+        } else if (val > max) {
+            val = max;
+        }
+        instance.update({
+            from: val
+        });
+    });
+// Язые
+    $( ".activelang" ).click(function() {
+        $( "#slideDown" ).slideToggle( function() {
+        });
+    });
 
 
 });
+
